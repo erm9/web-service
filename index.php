07e7c0614b0379108179ccdffed4d08adec9e95a
Ermiaas <ermiaasm@gmail.com>
2022-03-11 22:25:09 -0800

    final push

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>PHP Page!</title>
+</head>
+<body>
+    <h1>PHP Page!</h1>
+    <p>This is HTML</p>
+    <?php
+        echo "<p>This is PHP</p>";
+    ?>
+    <p>This is HTML</p>
+
+    <p>Hello from repl.it</p>
+
+</body>
+</html>
