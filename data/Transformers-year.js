607f21fd3354c599d7a44540eb71b6e692c75bdc
Ermiaas <ermiaasm@gmail.com>
2022-03-20 20:18:05 -0700

    final push

---
+++ data/Transformers-year.js
@@ -0,0 +1,82 @@
+{
+	"title": "Transformers Films by Year",
+	"films":[
+			  {
+				"Film":1,
+				"Title":"Transformers",
+				"Year":2007,
+				"Director":"Michael Bay",
+				"Producers":"Lorenzo di Bonaventura, Tom DeSanto, Don Murphy, and Ian Bryce",
+				"Writers":"Roberto Orci and Alex Kurtzman",
+				"Composer":"Steve Jablonsky",
+				"Starring":"Shia LaBeouf",
+				"Budget":"$147,000,000.00",
+				"BoxOffice":"$709,700,000.00",
+				"Image":"Transformers.jepg"
+			  },
+			  {
+				"Film":2,
+				"Title":"Revenge of the Fallen",
+				"Year":2009,
+				"Director":"Michael Bay",
+				"Producers":"Lorenzo di Bonaventura, Tom DeSanto, Don Murphy, and Ian Bryce",
+				"Writers":"Ehren Kruger, Roberto Orci and Alex Kurtzman",
+				"Composer":"Steve Jablonsky",
+				"Starring":"Shia LaBeouf",
+				"Budget":"$200,000,000.00",
+				"BoxOffice":"$863,300,000.00",
+				"Image":"Revenge-of-the-Fallen.jepg"
+			  {
+				"Film":3,
+				"Title":"Dark of the Moon",
+				"Year":2011,
+				"Director":"Michael Bay",
+				"Producers":"Lorenzo di Bonaventura, Tom DeSanto, Don Murphy, and Ian Bryce",
+				"Writers":"Ehren Kruger",
+				"Composer":"Steve Jablonsky",
+				"Starring":"Shia LaBeouf",
+				"Budget":"$195,000,000.00",
+				"BoxOffice":"$1,124,000,000.00",
+				"Image":"Dark-of-the-Moon.jepg"
+			  },
+			  {
+				"Film":4,
+				"Title":"Age of Extinction",
+				"Year":2014,
+				"Director":"Michael Bay",
+				"Producers":"Lorenzo di Bonaventura, Tom DeSanto, Don Murphy, and Ian Bryce",
+				"Writers":"Ehren Kruger",
+				"Composer":"Steve Jablonsky",
+				"Starring":"Mark Wahlberg",
+				"Budget":"$210,000,000.00",
+				"BoxOffice":"$1,104,000,000.00",
+				"Image":"Age-of-Extinction.jepg"
+			  },
+			  {
+				"Film":5,
+				"Title":"The Last Knight",
+				"Year":2017,
+				"Director":"Michael Bay",
+				"Producers":"Lorenzo di Bonaventura, Tom DeSanto, Don Murphy, and Ian Bryce",
+				"Writers":"Art Marcum, Matt Holloway and Ken Nolan",
+				"Composer":"Steve Jablonsky",
+				"Starring":"Mark Wahlberg",
+				"Budget":"$217,000,000.00",
+				"BoxOffice":"$605,400,000.00",
+				"Image":"The-Last-Knight.jepg"
+			  },
+			  {
+				"Film":6,
+				"Title":"Bumblebee",
+				"Year":2017,
+				"Director":"Travis Knight",
+				"Producers":"Lorenzo di Bonaventura, Tom DeSanto, Don Murphy, Michael Bay and Mark Vahradian",
+				"Writers":"Christina Hodson",
+				"Composer":"Dario Marianelli",
+				"Starring":"Hailee Steinfeld",
+				"Budget":"$102,000,000.00",
+				"BoxOffice":"$468,000,000.00",
+				"Image":"Bumblebee.png"
+			  },
+			]
+}
